6b7ed38c332f44e0900e70d3d128e823ef285e03
Phyllis <phyllisinma@gmail.com>
2024-12-19 16:50:51 +0800

    Initial commit from your app

--- src/main.js
+++ src/main.js
@@ -4,18 +4,13 @@ import './components/banner-carousel.js';
 import './components/solution-section.js';
 import './components/news-section.js';
 import './components/event-section.js';
+import './components/footer-section.js';
 
 class LayoutApp extends LitElement {
   static styles = css`
     :host {
       display: block;
       min-height: 100vh;
-    }
-
-    .footer {
-      background-color: #a0a0a0;
-      padding: 20px;
-      text-align: center;
     }
   `;
 
@@ -26,7 +21,7 @@ class LayoutApp extends LitElement {
       <solution-section></solution-section>
       <news-section></news-section>
       <event-section></event-section>
-      <div class="footer">Footer</div>
+      <footer-section></footer-section>
     `;
   }
 }
