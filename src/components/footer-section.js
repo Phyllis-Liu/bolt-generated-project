d39505f00217cbcdee5f31c83fc8d3a0114f30fd
Phyllis <phyllisinma@gmail.com>
2024-12-20 10:09:42 +0800

    Initial commit from your app

--- src/components/footer-section.js
+++ src/components/footer-section.js
@@ -55,6 +55,13 @@ class FooterSection extends LitElement {
       margin-bottom: 1rem;
     }
 
+    .contact-icon {
+      width: 24px;
+      height: 24px;
+      flex-shrink: 0;
+      fill: white;
+    }
+
     .social-links {
       display: flex;
       gap: 15px;
@@ -119,13 +126,13 @@ class FooterSection extends LitElement {
           <h3>INTERESTED IN OUR PRODUCTS AND SERVICES?</h3>
           <div class="contact-info">
             <p>
-              <svg class="social-icon" viewBox="0 0 24 24">
+              <svg class="contact-icon" viewBox="0 0 24 24">
                 <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
               </svg>
               11F, 114, SEC.1, XINTAI 5TH RD, XIZHI, 22102 NEW TAIPEI, TAIWAN
             </p>
             <p>
-              <svg class="social-icon" viewBox="0 0 24 24">
+              <svg class="contact-icon" viewBox="0 0 24 24">
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
               </svg>
               TEL: +886 2 2655 7997
